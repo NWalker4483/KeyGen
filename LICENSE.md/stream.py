c9e7bb62b7e377cdd3a77b83c5ffdbe54af92bb1
Nile Walker <niwal7@morgan.edu>
2017-11-30 08:25:37 -0800

    Add files via upload

---
+++ LICENSE.md/stream.py
@@ -0,0 +1,53 @@
+import cv2
+import time
+import datetime
+from imutils.video import VideoStream
+def Create_Views(windows,pos=(0,0)):#list of window names
+    for i in windows:
+        #Create Windows to view images
+        cv2.namedWindow(i, cv2.WINDOW_AUTOSIZE)  
+        cv2.moveWindow(i,0,0)  
+    #Start the window thread for the two windows we are using
+    cv2.startWindowThread()
+#Position the windows next to eachother
+def Update_Views(names,images):
+    for i in zip(names,images):
+        cv2.imshow(i[0],i[1])#name,image
+def ConnectCam(pi=False):
+    time.sleep(1)
+    # initialize the video stream and allow the cammera sensor to warmup
+    return VideoStream(usePiCamera=pi).start()
+    
+def Log_Image(name,image):
+    cv2.imwrite("{0}{1}.png".format(name,datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d-%H:%M:%S')),image)
+
+def pixelate(_image,pixelSize=32):
+        from PIL import Image
+        backgroundColor = (0,)*3
+        _image=Image.fromarray(_image)
+        _image = _image.resize((int(_image.size[0]/pixelSize), int(_image.size[1]/pixelSize)), Image.NEAREST)
+        _image = _image.resize((int(_image.size[0]*pixelSize), int(_image.size[1]*pixelSize)), Image.NEAREST)
+        pixel=_image.load()
+        for i in range(0,_image.size[0],pixelSize):
+            for j in range(0,_image.size[1],pixelSize):
+                for r in range(pixelSize):
+                    pixel[i+r,j] = backgroundColor
+                    pixel[i,j+r] = backgroundColor     
+        return np.array(_image)
+if __name__=="__main__":
+    camera=ConnectCam()
+    views=["Test"]
+    Create_Views(views)
+    while True:
+        key = cv2.waitKey(1) & 0xFF
+        _,img=camera.read()
+        if key=='q':
+            Log_Image("Test",img)
+        Update_Views(views,[img])
+    camera.release()
+    cv2.destroyAllWindows()
+
+
+
+#Start the window thread for the two windows we are using
+
