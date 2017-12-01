Intro to Pixel Scaling Notes: Correct Controlled Ratios and Build Key Template Classes + Input Method.

readblank.py
@@ -1,18 +1,39 @@
 from stl import mesh
-from mpl_toolkits import mplot3d
-from matplotlib import pyplot
+import numpy as np
+def Add_Temp(ridges):
+    temp=mesh.Mesh.from_file('lway.stl')
+    size=int((len(ridges.vectors)/6))
+    data = np.zeros(int(size//.2)*77, dtype=mesh.Mesh.dtype)
+    #i=0
+    #for a in [temp.vectors+[[j,0,0],[j,0,0],[j,0,0]] for j in np.arange(0,size,.2)]:
+    #    for b in a: 
+    #        data['vectors'][i]=b
+    #        i+=1
+    #    print(a.shape)
+    return mesh.Mesh(np.concatenate([
+    mesh.Mesh(data).data,ridges.data   
+]))
+if __name__ =="__main__":
+    from mpl_toolkits import mplot3d
+    from matplotlib import pyplot
+    from terrain import test_terra
+    # Create a new plot
+    figure = pyplot.figure()
+    axes = mplot3d.Axes3D(figure)
+    # Load the STL files and add the vectors to the plot
 
-# Create a new plot
-figure = pyplot.figure()
-axes = mplot3d.Axes3D(figure)
+    #[[23.7,14.6,1],[23.7,14.6,0],[23.7,20,1]]
+    ridge = test_terra(np.random.rand(30)*10+4.985438,zero=4.985438)
 
-# Load the STL files and add the vectors to the plot
-data = mesh.Mesh.from_file('Keys/Key_Blank_L.stl')
-axes.add_collection3d(mplot3d.art3d.Poly3DCollection(data.vectors[100:]))
+    #s1=[[23.7,14.6,1],[23.7,14.6,0],[23.7,20,1]]
 
-# Auto scale to the mesh size
-scale = data.points.flatten(-1)
-axes.auto_scale_xyz(scale, scale, scale)
+    key = Add_Temp(ridge)
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(key.vectors))
 
-# Show the plot to the screen
-pyplot.show()
+    # Auto scale to the mesh size
+    scale = key.points.flatten(-1)
+    axes.auto_scale_xyz(scale, scale, scale)
+
+    # Show the plot to the screen
+    pyplot.show()
+    key.save('ridges.stl')

terrain.py
@@ -1,22 +1,39 @@
 from stl import mesh
 import math
 import numpy
-def test_terra(y):
+
+def test_terra(y,index=0,zero=0):
     #length=35#mm
     length=len(y)
+    step=35/length
     # Create 3 faces of a cube
-    data = numpy.zeros(len(y), dtype=mesh.Mesh.dtype)
+    data = numpy.zeros(len(y)*6, dtype=mesh.Mesh.dtype)
     #a=int(input("How Far?"))
    #red,green,blue
+    y=[0]+y
     x=1
-    for i in range(len(data["vectors"]),2):
-        data['vectors'][i] = numpy.array([[x, 1, y[x-1]],
-                                    [x, 0, y[x-1]],
-                                    [x+1, 0, y[x]]])
-        data['vectors'][i+1] = numpy.array([[x+1, 1, y[x]],
-                                    [x+1, 0, y[x]],
-                                    [x, 1, y[x-1]]])
+    for i in range(0,len(data['vectors']),6):
+        data['vectors'][i] = numpy.array([[index, y[x-1],1],
+                                    [index, y[x-1], 0],
+                                    [index+step, y[x], 0]])
+        data['vectors'][i+1] = numpy.array([[index+step, y[x], 1],
+                                    [index+step, y[x], 0],
+                                    [index, y[x-1],1]])
+        data['vectors'][i+2] = numpy.array([[index, y[x-1], 1],
+                                    [index, zero, 1],
+                                    [index+step, zero,1]])
+        data['vectors'][i+3] = numpy.array([[index, y[x-1], 1],
+                                    [index+step, y[x], 1],
+                                    [index+step, zero,1]])
+        data['vectors'][i+4] = numpy.array([[index, y[x-1], 0],
+                                    [index, zero, 0],
+                                    [index+step, zero,0]])
+        data['vectors'][i+5] = numpy.array([[index, y[x-1], 0],
+                                    [index+step, y[x], 0],
+                                    [index+step, zero,0]])
         x+=1
+        index+=step
+    return mesh.Mesh(data)
     # Optionally render the rotated cube faces
 
 if __name__ == "__main__":
@@ -26,7 +43,7 @@ if __name__ == "__main__":
     figure = pyplot.figure()
     axes = mplot3d.Axes3D(figure)
     # Render the cube faces
-    data=mesh.Mesh(data)
+    data=test_terra(np.random.rand(10)*10+23.7)
     axes.add_collection3d(mplot3d.art3d.Poly3DCollection(data.vectors))
 
     # Auto scale to the mesh size