4077836ed49167fb283901f2d0d14cf891142464
Nile Walker <niwal7@morgan.edu>
2017-12-04 10:39:09 -0500

    Convert beta to read from stream

--- beta.py
+++ beta.py
@@ -1,21 +1,29 @@
-import stream as im
+import stream as see
 import cv2
 import os 
-import time
-from modelling import test_terra
-from modelling import top_edge
-import numpy as np
+from findkey import get_edge
+import modelling as make
 from stl import mesh
-from mpl_toolkits import mplot3d
-from matplotlib import pyplot
-from modelling import Add_Temp
-from modelling import plot_stl
-from modelling import KeyWay
-Key=KeyWay("L",35,5,8.521902)     
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument("-k","--keytype", type=str,help="The KeyWay")
+args = parser.parse_args()
+keyway=(args.keytype.upper() if args.keytype != None else "L")
+
+
+Key=make.KeyWay(keyway,35,5,8.521902)     
 # Load the STL files and add the vectors to the plot
-edge=cv2.imread('Logs/BestCase.png')
-edge=cv2.cvtColor(edge,cv2.COLOR_BGR2GRAY)
-#[[23.7,14.6,1],[23.7,14.6,0],[23.7,20,1]]
-key = Add_Temp(test_terra([i*Key.ridgemax for i in top_edge(edge)],Key=Key),Key)
-plot_stl(key)
+#edge=cv2.imread('Logs/BestCase.png')
+#edge=cv2.cvtColor(edge,cv2.COLOR_BGR2GRAY)
+cam=see.ConnectCam()
+views=["True"]
+see.Create_Views(views)
+while True: 
+    edge=get_edge(cam.read())
+    see.Update_Views(views,edge)
+    key=cv2.waitKey(1)
+    if key==ord("y"):
+        break
+key = make.Add_Temp(make.test_terra([i*Key.ridgemax for i in make.top_edge(edge)],Key=Key),Key)
+make.plot_stl(key)
 key.save('Keys/Key_{0}.stl'.format(len(os.listdir("Keys"))))
--- stream.py
+++ stream.py
@@ -10,9 +10,10 @@ def Create_Views(windows,pos=(0,0)):#list of window names
     #Start the window thread for the two windows we are using
     cv2.startWindowThread()
 #Position the windows next to eachother
-def Update_Views(names,images):
+def Update_Views(names,images,trigger=(None,None)):
     for i in zip(names,images):
         cv2.imshow(i[0],i[1])#name,image
+        cv2.waitKey(1)
 def ConnectCam(pi=False):
     cam=VideoStream(usePiCamera=pi).start()
     #Let Camera Warmup
